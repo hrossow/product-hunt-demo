e40960d18282d9e4d36fb59c930820fd7b20a1e9
heinrich.rossow <heinrich.rossow@zetrasoft.com.br>
2020-04-13 14:19:40 -0300

    Added iconic support. Added details to projects and home page

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, null=False, default="Title not parsed")    
+    pub_date = models.DateTimeField(default=timezone.datetime.now)
+    body = models.TextField(null=False, default="No description was supplied")
+    url = models.TextField(null=False)    
+    image = models.ImageField(upload_to="images/")
+    icon = models.ImageField(upload_to="images/")    
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+    def summary(self):
+        text=""
+        max_words=30
+        count=0
+        words=self.body.split(' ')
+        
+        if len(self.body.split(' ')) <= max_words:
+            return self.body
+        else:
+            while count < max_words:
+                text = "%s %s" % (text, words[count])
+                count += 1
+            return "{}...".format(text)
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %d %Y")
